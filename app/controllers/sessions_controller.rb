8f612931bca9c8a4292f70e2b426264153293900
anummithani <anum.mithani@kellogg.northwestern.edu>
2022-03-16 09:54:50 +0000

    login auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.where({ username: params["username"] })[0]
+    if @user
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session[:user_id] = @user.id
+        flash[:notice] = "Welcome."
+        redirect_to "/places"
+      else
+        flash[:notice] = "Unsuccessful login attempt. Please try again."
+        redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Unsuccessful login attempt. Please try again."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
